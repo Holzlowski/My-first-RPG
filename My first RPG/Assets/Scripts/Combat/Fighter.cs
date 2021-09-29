1e8a7673c86f532cad9c7e3bef01955c9607956f
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2021-09-29 17:15:12 +0200

    Start of Combat System
    - Prints something while attacking
    - Decoupling Dependencies between classes

---
+++ My first RPG/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,51 @@
+﻿using RPG.Core;
+using RPG.Movement;
+using System.Collections;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        [SerializeField]
+        float weaponRange = 2f;
+
+        Mover mover;
+        Transform target;
+
+        private void Start()
+        {
+            mover = GetComponent<Mover>();
+        }
+
+        private void Update()
+        {
+            if (target == null) return;
+
+            if (!GetIsInRange())
+            {
+                mover.MoveTo(target.position);
+            }
+            else
+            {
+                mover.Stop();
+            }
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+    }
+}
--- My first RPG/Assets/Scripts/Movement/Mover.cs
+++ My first RPG/Assets/Scripts/Movement/Mover.cs
@@ -1,46 +1,54 @@
-﻿using System.Collections;
+﻿using RPG.Combat;
+using RPG.Core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    [SerializeField] Transform target;
-    NavMeshAgent agent;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Mover : MonoBehaviour
     {
-        agent = GetComponent<NavMeshAgent>();
-    }
+        [SerializeField] Transform target;
+        NavMeshAgent agent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
+        // Start is called before the first frame update
+        void Start()
         {
-            MoveToCursor();
+            agent = GetComponent<NavMeshAgent>();
         }
-        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.yellow);
-        UpdateAnimator();
-    }
 
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit == true)
+        // Update is called once per frame
+        void Update()
         {
-            agent.SetDestination(hit.point);
+            UpdateAnimator();
         }
-    }
 
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            agent.destination = destination;
+            agent.isStopped = false;
+        }
+
+        public void Stop()
+        {
+            agent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
     }
 }
+
