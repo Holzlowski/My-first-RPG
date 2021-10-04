583f14351b673ed9e8da254c509b33b7bc580160
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2021-10-04 10:07:42 +0200

    added AI-Controller

--- My first RPG/Assets/Scripts/Combat/Fighter.cs
+++ My first RPG/Assets/Scripts/Combat/Fighter.cs
@@ -64,7 +64,7 @@ namespace RPG.Combat
             target.TakeDamage(weaponDamage);
         }
 
-        public bool CanAttack(CombatTarget combatTarget)
+        public bool CanAttack(GameObject combatTarget)
         {
             if (combatTarget == null) { return false; }
             Health targetToTest = combatTarget.GetComponent<Health>();
@@ -76,7 +76,7 @@ namespace RPG.Combat
             return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
-        public void Attack(CombatTarget combatTarget)
+        public void Attack(GameObject combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.GetComponent<Health>();
---
+++ My first RPG/Assets/Scripts/Controller/AIController.cs
@@ -0,0 +1,43 @@
+﻿using RPG.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        Fighter fighter;
+        GameObject player;
+
+        private void Start()
+        {
+            fighter = GetComponent<Fighter>();
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void Update()
+        {
+            
+            if(InAttackRangeOfPlayer() && fighter.CanAttack(player))
+            {
+                fighter.Attack(player);
+            }
+            else
+            {
+                fighter.Cancel();
+            }
+            
+        }
+
+        private bool InAttackRangeOfPlayer()
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            return distanceToPlayer < chaseDistance;
+        }
+
+    }
+}
+
