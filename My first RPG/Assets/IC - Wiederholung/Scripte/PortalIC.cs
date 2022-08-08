3a34f7ab185fdeb59b4117df9aa65670f5d826ae
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2022-08-08 20:33:02 +0200

    IC Leveltransition

---
+++ My first RPG/Assets/IC - Wiederholung/Scripte/PortalIC.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+using IC.SceneManagement;
+
+namespace IC.Scenemanagement
+{
+    public class PortalIC : MonoBehaviour
+    {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E, F
+        }
+
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeInTime = 2f;
+        [SerializeField] float fadeWaitTime = 0.5f;
+
+       private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            if(sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+
+            FaderIC fader = FindObjectOfType<FaderIC>();
+
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            PortalIC otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(PortalIC otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            //weil Spieler sonst irgendwo hinteleportiert wird, wegen dem NavMesh
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.position = otherPortal.spawnPoint.transform.position;
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
+        }
+
+        private PortalIC GetOtherPortal()
+        {
+            foreach(PortalIC portal in FindObjectsOfType<PortalIC>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+                return portal;
+            }
+            return null;
+        }
+    }
+}
+
+
